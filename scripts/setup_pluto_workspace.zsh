239388f6b2106de14e74aa2d49c5b6d47bddd029
Vittorio Lumare <vittorio.lumare@moley.com>
2020-05-09 13:39:26 +0100

    updated setup script

--- scripts/setup_pluto_workspace.zsh
+++ scripts/setup_pluto_workspace.zsh
@@ -1,3 +1,6 @@
 #!/bin/zsh
+# Add this line to to your .zsh config:
+#  alias pluto='. ~/ros/pluto_ws/src/pluto_robot/scripts/setup_pluto_workspace.zsh'
+
 source ~/ros/pluto_ws/devel/setup.zsh
 cd ~/ros/pluto_ws/src
