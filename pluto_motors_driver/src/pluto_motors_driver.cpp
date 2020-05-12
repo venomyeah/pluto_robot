4484d30485eb21594ba548622e81568c015109b7
Vittorio Lumare <venom@venom.it>
2020-05-12 19:29:56 +0000

    tidy up

--- pluto_motors_driver/src/pluto_motors_driver.cpp
+++ pluto_motors_driver/src/pluto_motors_driver.cpp
@@ -137,8 +137,6 @@ void PlutoMotorsDriver::read(const ros::Time &time,
   vel[1] = sign(eff_cmd[1]) * right_wheel_cycles_per_sec_ * M_PI * 2;
 #endif
 
-  vel[0] = 0.0;
-  vel[1] = 0.0;
 }
 
 void PlutoMotorsDriver::write(const ros::Time &time,
