9533c241970bb5c6dc81f2218d0f213db72c368a
Vittorio Lumare <vittorio.lumare@moley.com>
2020-12-19 16:07:33 +0000

    updated serial Tx

--- pluto_motors_driver/src/pluto_motors_driver.cpp
+++ pluto_motors_driver/src/pluto_motors_driver.cpp
@@ -66,11 +66,13 @@ bool serialOpen() {
 void serialClose() { close(serial_fd); }
 
 void serialTx(const std::string &str) {
-  // write(serial_fd, str.c_str(), sizeof(str.c_str())/sizeof(char));
-  std::stringstream ss;
-  ss << "echo \"" << str << "\" > /dev/ttyACM0";
-  system(ss.str().c_str());
+  write(serial_fd, str.c_str(), sizeof(str.c_str()) / sizeof(char));
+  // std::stringstream ss;
+  // ss << "echo \"" << str << "\" > /dev/ttyACM0";
+  // system(ss.str().c_str());
 }
+
+std::string serialRx() {}
 
 PlutoMotorsDriver::PlutoMotorsDriver() {
 
