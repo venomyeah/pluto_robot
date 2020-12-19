73cd73be046732118224c2f4e1c4db843d36ca1f
Vittorio Lumare <vittorio.lumare@moley.com>
2020-12-19 18:42:36 +0000

    updated diff drive config

--- pluto_joypad/include/pluto_joypad.hpp
+++ pluto_joypad/include/pluto_joypad.hpp
@@ -9,6 +9,7 @@
 #include <sensor_msgs/Joy.h>
 
 // messages
+#include <geometry_msgs/Twist.h>
 #include <std_msgs/Float64.h>
 
 // general
@@ -25,6 +26,7 @@ private:
   ros::Subscriber joy_sub_;
   ros::Publisher left_wheel_pub_;
   ros::Publisher right_wheel_pub_;
+  ros::Publisher twist_pub_;
 
   void joyCallback(const sensor_msgs::Joy joy_data);
 };
--- pluto_joypad/src/pluto_joypad.cpp
+++ pluto_joypad/src/pluto_joypad.cpp
@@ -14,22 +14,42 @@
 
 void PlutoJoypad::joyCallback(const sensor_msgs::Joy joy_data) {
 
-  auto left = joy_data.axes[1];
-  auto right = joy_data.axes[4];
+  if (0) {
+    // Direct Diff Drive
+    auto left = joy_data.axes[1];
+    auto right = joy_data.axes[4];
 
-  // noise removal
-  left = fabs(left) < 0.1 ? 0.0 : left;
-  right = fabs(right) < 0.1 ? 0.0 : right;
+    // noise removal
+    left = fabs(left) < 0.1 ? 0.0 : left;
+    right = fabs(right) < 0.1 ? 0.0 : right;
 
-  // conversion
-  std_msgs::Float64 left_cmd;
-  left_cmd.data = -left * 3;
-  std_msgs::Float64 right_cmd;
-  right_cmd.data = right * 3;
+    // conversion
+    std_msgs::Float64 left_cmd;
+    left_cmd.data = -left * 3;
+    std_msgs::Float64 right_cmd;
+    right_cmd.data = right * 3;
 
-  // pub
-  left_wheel_pub_.publish(left_cmd);
-  right_wheel_pub_.publish(right_cmd);
+    left_wheel_pub_.publish(left_cmd);
+    right_wheel_pub_.publish(right_cmd);
+  }
+
+  if (1) {
+    // twist
+    auto x = joy_data.axes[1]; // linear
+    auto z = joy_data.axes[3]; // angular
+
+    // noise removal
+    x = fabs(x) < 0.1 ? 0.0 : x;
+    z = fabs(z) < 0.1 ? 0.0 : z;
+
+    // conversion
+    geometry_msgs::Twist cmd_vel;
+    cmd_vel.linear.x = z * 0.3;
+    cmd_vel.angular.z = x * 1;
+
+    // pub
+    twist_pub_.publish(cmd_vel);
+  }
 }
 
 PlutoJoypad::PlutoJoypad() {
@@ -42,4 +62,6 @@ PlutoJoypad::PlutoJoypad() {
       "/pluto_motors/left_wheel_velocity_controller/command", 1000);
   right_wheel_pub_ = nh.advertise<std_msgs::Float64>(
       "/pluto_motors/right_wheel_velocity_controller/command", 1000);
+  twist_pub_ = nh.advertise<geometry_msgs::Twist>(
+      "/pluto_motors/diff_drive_controller/cmd_vel", 1000);
 }
